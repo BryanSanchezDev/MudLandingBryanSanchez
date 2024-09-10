78b5c09fe3931e037e5a5d7ee09d64b073862661
HeadshotCR <branbran911@hotmail.com>
2024-09-09 22:01:52 -0600

    Add project files.

---
+++ MudLandingBryanSanchez/wwwroot/typewriter.js
@@ -0,0 +1,12 @@
+﻿export function typeText() {
+    var app = document.getElementById('typing');
+
+    var typewriter = new Typewriter(app, {
+        loop: true
+    });
+
+    typewriter.typeString('Building Software At Startup Speed | .NET Full-Stack')
+        .pauseFor(5000)
+        .start();
+}
+
