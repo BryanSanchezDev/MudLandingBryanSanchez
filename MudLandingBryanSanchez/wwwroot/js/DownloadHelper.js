0761cea9a6872bc4e3489d7417eb02bbcb7dcf32
HeadshotCR <branbran911@hotmail.com>
2025-01-28 00:33:19 -0600

    feat: Use JS to add CV download

---
+++ MudLandingBryanSanchez/wwwroot/js/DownloadHelper.js
@@ -0,0 +1,8 @@
+﻿window.downloadCVFile = () => {
+    const url = '/files/BryanSanchezCV.pdf'; // Adjusted path to be relative to the root
+    const anchorElement = document.createElement('a');
+    anchorElement.href = url;
+    anchorElement.download = 'BryanSanchezCV.pdf';
+    anchorElement.click();
+    anchorElement.remove();
+}
